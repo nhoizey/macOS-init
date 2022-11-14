e43a854f3222acd90d8a2271de7711769db28609
Nicolas Hoizey <nicolas@hoizey.com>
2022-11-14 10:28:48 +0100

    Update Homebrew install command

--- run-first.sh
+++ run-first.sh
@@ -19,7 +19,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 if test ! $(which brew)
 then
   echo "Installation de Homebrew"
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Ajout des binaires Homebrew au PATH
